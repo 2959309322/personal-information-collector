7441a555abca0bcd6b2103039d6c585930ef2d8e
yxh <yexinhao1@mails.gdut.edu.cn>
2026-01-27 14:09:24 +0800

    111

---
+++ src/collector/base.py
@@ -0,0 +1,25 @@
+import time
+from functools import wraps
+def timer(func):
+    @wraps
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            gap = time.time() - start
+            print(f"doing {func.__name__} using time: {gap:.2f} seconds")
+    return wrapper
+
+def retry(max_retry: int = 3,delay: float = 1.0):
+    def decorator(func):
+        @wraps
+        def wrapper(*args, **kwargs):
+            for i in range(max_retry):
+                try:
+                    return func(*args, **kwargs)
+                finally:
+                    print(f"the {i} time to retry {func.__name__} but failed")
+                    time.sleep(delay)
+        return wrapper
+    return decorator
